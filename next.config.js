75729ca59655c06ae94d741e3e66729907b9117a
smyaseen <smyaseen164@gmail.com>
2023-05-16 23:34:00 +0500

    ignored typescript build errors

--- next.config.js
+++ next.config.js
@@ -16,4 +16,11 @@ module.exports = withPWA({
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 });
