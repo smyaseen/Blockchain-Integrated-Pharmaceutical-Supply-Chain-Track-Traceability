da32fe9cac70e0c2c7a7f35f7eb68e06089d7cc8
smyaseen <smyaseen164@gmail.com>
2023-06-09 16:29:57 +0500

    changed footer name

--- src/routes/Layout/index.tsx
+++ src/routes/Layout/index.tsx
@@ -62,7 +62,7 @@ const Layout = ({ children }: Props) => {
             </Box>
           </DashboardLayoutRoot>
           <Typography align="center">
-            <CopyrightRounded /> {new Date().getFullYear()}, made by SMY & HFAK{' '}
+            <CopyrightRounded /> {new Date().getFullYear()}, made by SMY & TEAM{' '}
             <RocketLaunchOutlined />
           </Typography>
           {status === 'authenticated' && (
